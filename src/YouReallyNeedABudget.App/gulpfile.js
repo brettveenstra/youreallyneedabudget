e0c535a4e6c4fa9260275caf9a23781ac62d87bc
Joe Petrakovich <jpetrakovich@gmail.com>
2016-10-13 21:32:47 -0400

    removed vs2015 app project in favor of vscode. added initial JS development stack (gulp, browserify, react).

--- src/YouReallyNeedABudget.App/gulpfile.js
+++ src/YouReallyNeedABudget.App/gulpfile.js
@@ -1,24 +1,14 @@
-﻿/*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
-
 var gulp = require('gulp');
 var browserify = require('browserify');
-var reactify = require('reactify');
-var source = require('vinyl-source-stream'); //use conventional text streams with Gulp
-var concat = require('gulp-concat'); //concats files
+var babelify = require('babelify');
+var source = require('vinyl-source-stream'); 
 
 var config = {
     paths: {
         html: './src/*.html',
         js: './src/**/*.js',
-        css: [
-            'node_modules/bootstrap/dist/css/bootstrap.min.css',
-            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
-        ],
-        wwwroot: './wwwroot',
-        mainJS: './src/main.js'
+        mainJS: './src/main.jsx',
+        wwwroot: './wwwroot'
     }
 }
 
@@ -29,17 +19,11 @@ gulp.task('html', function () {
 
 gulp.task('js', function () {
     browserify(config.paths.mainJS)
-    .transform(reactify)
+    .transform("babelify", {presets: ["react"]})
     .bundle()
     .on('error', console.error.bind(console))
     .pipe(source('bundle.js'))
     .pipe(gulp.dest(config.paths.wwwroot + '/scripts'));
 });
 
-gulp.task('css', function () {
-    gulp.src(config.paths.css)
-    .pipe(concat('bundle.css'))
-    .pipe(gulp.dest(config.paths.wwwroot + '/css'));
-});
-
-gulp.task('default', ['html', 'css']);
+gulp.task('default', ['html', 'js']);
---
+++ src/YouReallyNeedABudget.App/src/components/homePage.jsx
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>YouReallyNeedABudget</h1>
+                <p>A simple budgeting tool built with ASP.NET Core and React.</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
